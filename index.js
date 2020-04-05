b29c275cb1e0b6ca50a594650d90abf5289735c8
steffenmalisi <steffenmalisi@users.noreply.github.com>
2020-04-05 19:23:07 +0200

    Updated diagramr to be used as CLI tool

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
+#!/usr/bin/env node
 'use strict';
 
 const PumlService = require('./puml.service');
 const pumlService = new PumlService();
+const program = require('commander');
 
-const csvPath = 'input.csv';
-const templatePath = 'puml.template';
-const outputPath = 'output.puml';
+program
+  .arguments('<csv>')
+  .option(
+    '-t, --template <template>',
+    'The template file to use. If not specified the a shipped default template is used.'
+  )
+  .option(
+    '-o, --output <output>',
+    'The output path to be used. If not specified it will default to output.puml in your current directory.'
+  )
+  .action(function(csv) {
+    const template = getTemplatePathOrDefault(program.template);
+    const output = getOutputPathOrDefault(program.output);
+    pumlService.createPumlFromCsv(csv, template, output);
+  })
+  .parse(process.argv);
 
-pumlService.createPumlFromCsv(csvPath, templatePath, outputPath);
+/**
+ * Checks if the given path is valid. If true it returns this path.
+ * If not, it returns the default.
+ * @param {String} templatePath
+ * @return {String} valid template path
+ */
+function getTemplatePathOrDefault(templatePath) {
+  return getOrDefault(templatePath, 'puml.template');
+}
+
+/**
+ * Checks if the given path is valid. If true it returns this path.
+ * If not, it returns the default.
+ * @param {String} outputPath
+ * @return {String} valid output path
+ */
+function getOutputPathOrDefault(outputPath) {
+  return getOrDefault(outputPath, 'output.puml');
+}
+
+/**
+ * Checks if the given path is valid. If true it returns this path.
+ * If not, it returns the default.
+ * @param {String} path
+ * @param {String} defaultPath
+ * @return {String} valid output path
+ */
+function getOrDefault(path, defaultPath) {
+  if (path == null) {
+    // TODO check path
+    return defaultPath;
+  } else {
+    return path;
+  }
+}
